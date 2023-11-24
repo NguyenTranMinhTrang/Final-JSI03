6fcbb6c63ff713b8d85957a0eeb2e0df08fa8cc1
Seo Bao Cuong <135335013+SeoBaoCuong@users.noreply.github.com>
2023-11-24 20:11:00 +0700

    We got new "things"

--- static/js/views/LoginView.js
+++ static/js/views/LoginView.js
@@ -8,7 +8,35 @@ export default class extends ClassView {
 
     async getHtml() {
         return `
-            <h2>Login jbjhv</h2>
+        <div class="login">
+        <div class="login-container">
+            <div class="login-name">
+                <h2>Log In</h2>
+            </div>
+    
+            <div class="login-email">
+                <input type="email" placeholder="Email">
+            </div>
+    
+            <div class="login-password">
+                <input type="password" placeholder="Password">
+            </div>
+
+            <div class="login-twosmallthings">
+                <input type="checkbox">
+                <label for="checkbox">Remember Me</label>
+
+                <div class="login-forgotPassword">
+                    <p>Forgort Password?</p>
+                </div>
+            </div>
+        
+
+            <div class="login-submit">
+                <button>Submit</button>
+            </div>
+        </div>
+    </div>
         `;
     }
 }
--- static/js/views/RegisterView.js
+++ static/js/views/RegisterView.js
@@ -8,7 +8,32 @@ export default class extends ClassView {
 
     async getHtml() {
         return `
-            <h1>Register</h1>
+        <div class="register">
+        <div class="register-container">
+            <div class="register-name">
+                <h2>Register</h2>
+            </div>
+    
+            <div class="register-email">
+              
+                <input type="email" placeholder="Email">
+            </div>
+    
+            <div class="register-password">
+               
+                <input type="password" placeholder="Password">
+            </div>
+    
+            <div class="register-confirmPassword">
+               
+                <input type="password" placeholder="Confirm Password">
+            </div>
+    
+            <div class="register-submit">
+                <button>Submit</button>
+            </div>
+        </div>
+    </div>
         `;
     }
 }
