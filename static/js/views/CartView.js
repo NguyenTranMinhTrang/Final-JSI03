0abdf408fdc638fb77e177db7c1253e13aecb8cc
Long xã đoàn <135336035+Longzata@users.noreply.github.com>
2023-12-22 18:52:40 +0700

    fix new cart page ui

--- static/js/views/CartView.js
+++ static/js/views/CartView.js
@@ -9,77 +9,77 @@ export default class extends ClassView {
     async getHtml() {
         return `
         <div class="cart-all">
-            <div class="cart-container">
-                <div class="cart-shopping">
-                    <div class="cart-shopping-navbar">
-                        <h1>Shopping Cart</h1>
-                        <h2 id="cart-quality">0 Items</h2>
+            <div class="cart-shopping">
+                <div class="cart-shopping-navbar">
+                    <h1>Shopping Cart</h1>
+                    <h2 id="cart-quality">0 Items</h2>
+                </div>
+
+                <div class="cart-lines"></div>
+
+                <div class="cart-products">
+                    <div class="cart-header">
+                        <div class="cart-header-title">
+                            <p><b>PRODUCT DETAILS</b></p>
+                        </div>
+
+                        <div class="cart-header-detail">
+                            <div class="cart-detail-item">
+                                <p><b>QUANTITY</b></p>
+                            </div>
+
+                            <div class="cart-detail-item">
+                                <p><b>PRICE</b></p>
+                            </div>
+
+                            <div class="cart-detail-item">
+                                <p><b>TOTAL</b></p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="cart-lines"></div>
-
-                    <div class="cart-products">
-                        <div class="cart-header">
-                            <div class="cart-header-title">
-                                <p><b>PRODUCT DETAILS</b></p>
-                            </div>
-
-                            <div class="cart-header-detail">
-                                <div class="cart-detail-item">
-                                    <p><b>QUANTITY</b></p>
-                                </div>
-
-                                <div class="cart-detail-item">
-                                    <p><b>PRICE</b></p>
-                                </div>
-
-                                <div class="cart-detail-item">
-                                    <p><b>TOTAL</b></p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div id="cart-container-item">
-                        </div>
+                    <div id="cart-container-item">
+                    </div>
                     <div class="cart-return">
                         <i class="fa-solid fa-arrow-left"></i>
                         <a href="">Continue Shopping</a>
                     </div>
                 </div>
 
-                <div class="cart-order-summary">
-                    <div class="cart-summary">
-                        <h1>Oder summary</h1>
-                    </div>
+                
+            </div>
+            <div class="cart-order-summary">
+                <div class="cart-summary">
+                    <h1>Oder summary</h1>
+                </div>
 
-                    <div class="cart-lines-second"></div>
+                <div class="cart-lines-second"></div>
 
-                    <div class="cart-sale-card">
-                        <h3>Subtotal</h3>
-                        <h4 id="cart-total">$1.08</h4>
-                    </div>
+                <div class="cart-sale-card">
+                    <h3>Subtotal</h3>
+                    <h4 id="cart-total">$1.08</h4>
+                </div>
 
-                    <div class="cart-sale-card">
-                        <h3>Estimated Shipping & Handing</h3>
-                        <h4>$3.04</h4>
-                    </div>
+                <div class="cart-sale-card">
+                    <h3>Estimated Shipping & Handing</h3>
+                    <h4>$3.04</h4>
+                </div>
 
-                    <div class="cart-sale-card">
-                        <h3>Estimated Tax</h3>
-                        <h4>__</h4>
-                    </div>
+                <div class="cart-sale-card">
+                    <h3>Estimated Tax</h3>
+                    <h4>__</h4>
+                </div>
 
-                    <div class="cart-lines-second"></div>
+                <div class="cart-lines-second"></div>
 
-                    <div class="cart-total">
+                <div class="cart-total">
 
-                    </div>
+                </div>
 
-                    <div class="cart-lines-second"></div>
+                <div class="cart-lines-second"></div>
 
-                    <div class="cart-checkout">
+                <div class="cart-checkout">
 
-                    </div>
                 </div>
             </div>
         </div>
